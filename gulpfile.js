3ec1c542f1fba6a2434e4fb8d7e7f9f60d0dc4ba
Lubos Krnac <lubos.krnac@gmail.com>
2017-01-07 22:17:10 +0530

    Switch to double-quotes
    To match style from my work

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,56 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const git = require('gulp-git');
-const runSequence = require('run-sequence');
-const shell = require('gulp-shell');
-const fs = require('fs');
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const git = require("gulp-git");
+const runSequence = require("run-sequence");
+const shell = require("gulp-shell");
+const fs = require("fs");
 
-gulp.task('lint', () => {
-  return gulp.src('gulpfile.js')
+gulp.task("lint", () => {
+  return gulp.src("gulpfile.js")
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('pull-server', (callback) => {
-  if (fs.existsSync('./janodemp-server/.git')) {
-    git.pull('origin', 'master', {cwd: 'janodemp-server'}, callback);
+gulp.task("pull-server", (callback) => {
+  if (fs.existsSync("./janodemp-server/.git")) {
+    git.pull("origin", "master", {cwd: "janodemp-server"}, callback);
   } else {
-    git.clone('https://github.com/lkrnac/janodemp-server.git', {cwd: '.'}, callback);
+    git.clone("https://github.com/lkrnac/janodemp-server.git", {cwd: "."}, callback);
   }
 });
 
-gulp.task('pull-client', (callback) => {
-  if (fs.existsSync('./janodemp-client/.git')) {
-    git.pull('origin', 'master', {cwd: 'janodemp-client'}, callback);
+gulp.task("pull-client", (callback) => {
+  if (fs.existsSync("./janodemp-client/.git")) {
+    git.pull("origin", "master", {cwd: "janodemp-client"}, callback);
   } else {
-    git.clone('https://github.com/lkrnac/janodemp-client.git', {cwd: '.'}, callback);
+    git.clone("https://github.com/lkrnac/janodemp-client.git", {cwd: "."}, callback);
   }
 });
 
-gulp.task('cd-client', (callback) => {
-  process.chdir('../janodemp-client');
+gulp.task("cd-client", (callback) => {
+  process.chdir("../janodemp-client");
   callback();
 });
 
-gulp.task('cd-server', (callback) => {
-  process.chdir('janodemp-server');
+gulp.task("cd-server", (callback) => {
+  process.chdir("janodemp-server");
   callback();
 });
 
-gulp.task('build-client', shell.task(['pwd', 'npm i', 'ng build --prod']));
-gulp.task('build-server', shell.task(['pwd', 'npm i']));
+gulp.task("build-client", shell.task(["pwd", "npm i", "ng build --prod"]));
+gulp.task("build-server", shell.task(["pwd", "npm i"]));
 
-gulp.task('default', (callback) => {
+gulp.task("default", (callback) => {
   runSequence(
-    'lint',
-    ['pull-server', 'pull-client'],
-    'cd-server',
-    'build-server',
-    'cd-client',
-    'build-client',
+    "lint",
+    ["pull-server", "pull-client"],
+    "cd-server",
+    "build-server",
+    "cd-client",
+    "build-client",
     callback
   );
 });
